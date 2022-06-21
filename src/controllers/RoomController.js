da4aa8062faeda5bef4314b10b7e7b4d272763b2
Rodolpho Toppan <rodolphotoppan@hotmail.com>
2022-06-20 21:52:40 -0300

    initial commit

---
+++ src/controllers/RoomController.js
@@ -0,0 +1,41 @@
+const Database = require('../db/config')
+
+module.exports = {
+  async create(req, res) {
+    const db = await Database()
+    const pass = req.body.password
+    let roomId
+    let isRoom = true
+
+    while (isRoom) {
+      // gera o número da sala
+      for (var i = 0; i < 6; i++) {
+        i == 0
+          ? (roomId = Math.floor(Math.random() * 10).toString())
+          : (roomId += Math.floor(Math.random() * 10).toString())
+      }
+
+      // verificar se o número já existe
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+      isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+      if (!isRoom) {
+        // insere a sala no db
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+        )`)
+      }
+    }
+
+    await db.close()
+
+    res.redirect(`/room/${roomId}`)
+  },
+  open(req, res) {
+    const roomId = req.params.room
+    res.render('room', { roomId: roomId })
+  }
+}
---
+++ src/route.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
+const RoomCrontroller = require('./controllers/RoomController')
+
+const route = express.Router()
+
+route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
+route.get('/create-pass', (req, res) =>
+  res.render('index', { page: 'create-pass' })
+)
+
+route.post('/create-room', RoomCrontroller.create)
+route.get('/room/:room', RoomCrontroller.open)
+
+route.post('/question/create', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
+
+module.exports = route
